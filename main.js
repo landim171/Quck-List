600e30283a3097b8c0016b507c7a09a9f7145e8a
landim171 <daniel.landim@eaportal.org>
2025-06-04 22:17:18 -0300

    18 commit

--- main.js
+++ main.js
@@ -1,44 +1,61 @@
 const items = []
 
-function addItem() {
-    const itemName = document.querySelector("#item").ariaValueMax
+function addItem(){
+    const itemName = document.querySelector("#item").value.trim()
+    if (!itemName) return
+
     const item = {
         name: itemName,
         checked: false
     }
 
     items.push(item)
-
     document.querySelector("#item").value = ""
-
-     showItemList()
-
+    showItemsList()
 }
 
-function showItemList() {
+function showItemsList(){
     const sectionList = document.querySelector(".list")
-    sectionList.textContent = ""
+    sectionList.innerHTML = ""
 
-    items.map((item, index) => {
-        sectionList.innerHTML += '
-         <div class="item">
-                <div>
-                    <input type="checkbox" name="list" id="item-${index}">
-                    <div class="custom-checkbox">
-                        <img src="./assets/checked.svg" alt="checkbox">
-                        <div>
-                            <label for="item-${index}">${item.name}</label>
-                        </div>
-
-                    </div>
+    items.forEach((item, index) => {
+        sectionList.innerHTML += `
+        <div class="item">
+            <div>
+                <input type="checkbox" id="item-${index}" ${item.checked ? "checked" : ""} onclick="checkItem(${index})"> 
+                <div class="custom-checkbox" onclick="checkItem(${index})">
+                    <img src="assets/checked.svg" alt="checked">
                 </div>
-                <div>
-                    <button>
-                        <img src="./assets/trash-icon.svg" alt="trash trash-icon">
-                    </button>
-                </div>
+                <label for="item-${index}" onclick="checkItem(${index})" class="${item.checked ? 'checked' : ''}">
+                    ${item.name}
+                </label>
             </div>
-
-        '
+            <button onclick="removeItem(${index})">
+                <img src="assets/trash-icon.svg" alt="trash icon">
+            </button>
+        </div>
+        `
     })
 }
+
+function checkItem(index){
+    items[index].checked = !items[index].checked
+    showItemsList()
+}
+
+function removeItem(index){
+    const divWarning = document.querySelector(".warning")
+    divWarning.classList.remove("hide-warning")
+    
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
+
+    items.splice(index, 1)
+    showItemsList()
+}
+
+function addHideWarningClass(){
+    document.querySelector(".warning").classList.add("hide-warning")
+}
+ 
